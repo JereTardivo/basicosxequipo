cc0381aa0ac73cc56a47a70cd41737cbca7a3db9
Jeremias Tardivo <jeretardivo1992@gmail.com>
2025-06-23 15:33:39 -0300

    titulo pestaña

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Basicos - Flexxus</title>
+        <meta name="description" content="Gestión de llamadas..." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
